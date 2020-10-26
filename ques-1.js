f837bad9766b5225f975a950e9b9c61a2a16a27b
Naem Bhuiyan <hopingboynaem@gmail.com>
2020-10-26 21:46:03 +0600

    modified 1

--- ques-1.js
+++ ques-1.js
@@ -1,9 +1,7 @@
 function customPrint(n, message) {
-  console.log(message);
   //  Add your code here
-  if (n > 0) {
-    return customPrint(n - 1, message);
+  if (n > 0 && n !== 0) {
+    console.log(message);
+    customPrint(n - 1, message);
   }
 }
-
-console.log(customPrint(5, "Hello, World!"));
